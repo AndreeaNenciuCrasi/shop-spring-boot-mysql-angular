653513f44d9fc203eb6bf3cb00badc053c4282b2
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-07-19 13:08:41 +0300

    first commit

---
+++ 02-shop-backend/src/main/java/com/example/shop/Product/repo/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.shop.Product.repo;
+
+import com.example.shop.Product.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Repository
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
